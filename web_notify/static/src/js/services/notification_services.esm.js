f50b98926ea84a00e55520f553586218404666ce
arobertlobo5 <arobertlobo5@gmail.com>
2023-12-09 16:02:16 -0500

    [MIG] Migrate web_notify to Odoo 17.0.

---
+++ web_notify/static/src/js/services/notification_services.esm.js
@@ -0,0 +1,44 @@
+/** @odoo-module **/
+import {renderToMarkup} from "@web/core/utils/render";
+import {browser} from "@web/core/browser/browser";
+import {registry} from "@web/core/registry";
+
+export const webNotificationService = {
+    dependencies: ["bus_service", "notification"],
+
+    start(env, {bus_service, notification}) {
+        let webNotifTimeouts = {};
+        /**
+         * Displays the web notification on user's screen
+         */
+
+        function displaywebNotification(notifications) {
+            Object.values(webNotifTimeouts).forEach((notif) =>
+                browser.clearTimeout(notif)
+            );
+            webNotifTimeouts = {};
+
+            notifications.forEach(function (notif) {
+                browser.setTimeout(function () {
+                    notification.add(renderToMarkup(notif.message), {
+                        title: notif.title,
+                        type: notif.type,
+                        sticky: notif.sticky,
+                        className: notif.className,
+                    });
+                });
+            });
+        }
+
+        bus_service.addEventListener("notification", ({detail: notifications}) => {
+            for (const {payload, type} of notifications) {
+                if (type === "web.notify") {
+                    displaywebNotification(payload);
+                }
+            }
+        });
+        bus_service.start();
+    },
+};
+
+registry.category("services").add("webNotification", webNotificationService);
